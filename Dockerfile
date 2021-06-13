f7ddcbf6d920658408966b45def8d6c63f530cc7
backendAlgo <mirshod.mirjonov99@mail.ru>
2021-06-13 21:30:39 +0500

    till redux

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:14.17.0-alpine
+WORKDIR /app
+ENV PATH /app/node_modules/.bin:$PATH
+COPY package*.json ./
+RUN npm install --silent
+COPY . ./ 
+CMD [ "npm", "start" ]
---
+++ src/store/dark-mode/reducer.test.ts
@@ -0,0 +1,16 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import darkModeReducer, { initialState, toggleDarkMode } from "./reducer";
+
+describe("dark mode reducer", () => {
+    test("should return the initial state if no action is provided", () => {
+        expect(darkModeReducer(undefined, {} as PayloadAction)).toEqual(
+            initialState
+        );
+    });
+    test("should enable dark mode if it is disabled", () => {
+        expect(darkModeReducer(false, toggleDarkMode)).toEqual(true);
+    });
+    test("should disable dark mode if it is enables", () => {
+        expect(darkModeReducer(true, toggleDarkMode)).toEqual(false);
+    });
+});
